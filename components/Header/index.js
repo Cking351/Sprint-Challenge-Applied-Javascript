cc6c6b3b9557afaac2c3d8051ed05f751ee2a7cb
Chris King <cking5836@gmail.com>
2020-06-06 19:59:48 -0400

    Accidently made PR to master

--- components/Header/index.js
+++ components/Header/index.js
@@ -18,5 +18,13 @@ function Header() {
   const h1Title = document.createElement('h1')
   const subspan = document.createElement('span')
 
-  
+  // Assign Classes
+  subDiv.classList.add('header')
+  mainSpan.classList.add('date')
+  subspan.classList.add('temp')
+
+  // Create text content
+  mainSpan.textContent = "SMARCH 28, 2019"
+  h1Title.textContent = 'Lambda Times'
+  subspan.textContent = '98°'
 }
